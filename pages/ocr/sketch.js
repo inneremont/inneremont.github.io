6abafa10633589d76abdbe7e248a0f314de51334
inneremont <115476629+inneremont@users.noreply.github.com>
2022-12-02 04:49:46 +0200

    Update sketch.js

--- pages/ocr/sketch.js
+++ pages/ocr/sketch.js
@@ -50,6 +50,7 @@ function draw() {
 		ellipse(windowWidth/2,windowHeight*.7,width/3);
 		fill(255)
 		textSize(20);
+		textWrap(WORD);
 		text("Find the words scan with button for surprise!", windowWidth/2,windowHeight*.7)
 
 	}
