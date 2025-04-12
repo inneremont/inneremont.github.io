a82adc25bd90e6cf070bcbe51cdc8f4d8b8b9ee9
inneremont <jbasandberg@gmail.com>
2025-04-13 01:04:51 +0300

    type tester update

--- typetester/script.js
+++ typetester/script.js
@@ -1,3 +1,17 @@
+document.getElementById('caps-toggle').addEventListener('change', function () {
+  const isChecked = this.checked;
+  document.querySelectorAll('.sample').forEach(el => {
+    el.style.textTransform = isChecked ? 'uppercase' : 'none';
+  });
+});
+
+
+document.getElementById('dark-toggle').addEventListener('change', function () {
+  document.body.classList.toggle('dark-mode', this.checked);
+});
+
+
+
 function updateSize(newVal) {
   var newFontSize = newVal + 'px';
   document.querySelectorAll('.sample').forEach(el => {
